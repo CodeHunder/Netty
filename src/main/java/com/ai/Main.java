d6970840e0d543ed841ef14f3e3b46fd992be80b
zining <zining@RHT.com>
2018-03-20 11:01:23 +0800

    实现websocket 通信

--- src/main/java/com/ai/Main.java
+++ src/main/java/com/ai/Main.java
@@ -9,13 +9,20 @@ import io.netty.channel.nio.NioEventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * @Author ZINING
  * @Description 程序入口，负责启动应用
  * @Date Create in 2018/2/26 15:43
  */
 public class Main {
-    public static void main(String[] args) {
+
+    /*public static void main(String[] args) {
+        Thread thread=new Thread();
+        thread.start();
+        System.out.println(System.getProperty("java.vm.name"));
+        System.out.println(System.getProperty("java.vm.info"));
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workGroup = new NioEventLoopGroup();
         try {
@@ -33,5 +40,20 @@ public class Main {
             bossGroup.shutdownGracefully();
             workGroup.shutdownGracefully();
         }
+    }*/
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        byte[] bytes = new byte[16];
+        bytes[0] = 125;
+        bytes[1] = 20;
+        bytes[2] = 119;
+        bytes[3] = 38;
+        for (int i = 4; i < bytes.length; i++) {
+            bytes[i] = 0;
+        }
+        System.out.println("byte值" + bytes.toString());
+        String s=new String(bytes,"ISO-8859-1");
+        String s2=new String(bytes,"UTF-8");
+        System.out.println( "s"+s);
+        System.out.println( "s1"+s2);
     }
 }
